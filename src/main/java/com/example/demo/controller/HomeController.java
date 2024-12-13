1b6ec5acc1d412d47a5c4ffa0ea06f726f909c0a
fusatsukatsujin <fusatsukatsujin@gmail.com>
2024-12-13 13:58:04 +0900

    dynamodbにデータを登録できるところまで

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,35 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import java.util.Map;
 
 @Controller
 public class HomeController {
     
+    @Autowired
+    private DynamoDbClient dynamoDbClient;
+    
     @GetMapping("/")
     public String home() {
+        return "index";
+    }
+
+    @PostMapping("/register")
+    public String register() {
+        dynamoDbClient.putItem(PutItemRequest.builder()
+        .tableName("Users")
+        .item(Map.of(
+            "id", AttributeValue.builder().s("1").build(),
+            "name", AttributeValue.builder().s("John Doe").build()
+        ))
+        .build());
         return "index";
     }
 } 
