a2634f8511e4976c39279ee99628f1b50aa0842c
fusatsukatsujin <fusatsukatsujin@gmail.com>
2024-12-16 09:19:24 +0900

    accesskeyの設定は不要だった

--- src/main/java/com/example/demo/config/DynamoDBConfig.java
+++ src/main/java/com/example/demo/config/DynamoDBConfig.java
@@ -3,11 +3,9 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import java.net.URI;
 
 @Configuration
@@ -16,19 +14,10 @@ public class DynamoDBConfig {
     @Value("${amazon.dynamodb.endpoint}")
     private String dynamodbEndpoint;
 
-    @Value("${amazon.dynamodb.accessKey}")
-    private String accessKey;
-
-    @Value("${amazon.dynamodb.secretKey}")
-    private String secretKey;
-
-
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
                 .endpointOverride(URI.create(dynamodbEndpoint))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .region(Region.AP_NORTHEAST_1)
                 .build();
     }
